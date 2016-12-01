88b6f89a3404c56e30fcf1c4438095633f2af68e
Syed Aman <sna274@nyu.edu>
2016-11-30 20:59:58 -0500

    remove some close slashes

--- register.php
+++ register.php
@@ -4,11 +4,11 @@
     </head>
     <body>
         <h2>Registration Page</h2>
-        <a href="index.php">Click here to go back<br/><br/>
+        <a href="index.php">Click here to go back<br><br>
         <form action="register.php" method="POST">
-            Enter Username: <input type="text" name="username" required="required" /> <br/>
-            Enter Password: <input type="password" name="password" required="required" /> <br/>
-            <input type="submit" value="Register"/>
+            Enter Username: <input type="text" name="username" required="required"> <br>
+            Enter Password: <input type="password" name="password" required="required"> <br>
+            <input type="submit" value="Register">
         </form>
     </body>
 </html>
