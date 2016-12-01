58568514361cea5de06acba46e8a62361e29ed92
Syed Aman <sna274@nyu.edu>
2016-12-01 05:26:23 -0500

    use php to access and add to MySQL database

--- register.php
+++ register.php
@@ -1,3 +1,5 @@
+<!-- User Registration Page -->
+
 <html>
     <head>
         <title>PHP Register Page</title>
@@ -5,6 +7,7 @@
     <body>
         <h2>Registration Page</h2>
         <a href="index.php">Click here to go back<br><br>
+        <!-- Send POST HTTP request -->
         <form action="register.php" method="POST">
             Enter Username: <input type="text" name="username" required="required"> <br>
             Enter Password: <input type="password" name="password" required="required"> <br>
@@ -12,3 +15,18 @@
         </form>
     </body>
 </html>
+
+<!-- Access database and add registered users -->
+<?php
+    // Look for REQUEST_METHOD in $SERVER array created by Apache web server and see if it matches with POST requests
+    if($SERVER["REQUEST_METHOD"] == "POST" {
+        // Escape strings to add to safely add to MySQL query
+        // Add unescaped strings to POST request
+        $username = mysql_real_escape_string($_POST['username']);
+        $password = mysql_real_escape_string($_POST['password']);
+
+        // Log user registration info with concatination
+        echo "Username entered is " . $username . "<br>";
+        echo "Password entered is " . $password . "<br>";
+    }
+?>
