0c07973fc6ade64e9c8e26fb14147c998f72f49a
Syed Aman <sna274@nyu.edu>
2016-11-30 20:56:27 -0500

    define register page

---
+++ register.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>PHP Register Page</title>
+    </head>
+    <body>
+        <h2>Registration Page</h2>
+        <a href="index.php">Click here to go back<br/><br/>
+        <form action="register.php" method="POST">
+            Enter Username: <input type="text" name="username" required="required" /> <br/>
+            Enter Password: <input type="password" name="password" required="required" /> <br/>
+            <input type="submit" value="Register"/>
+        </form>
+    </body>
+</html>
