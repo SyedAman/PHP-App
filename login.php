c4ff3ed75bfc6deaece708ea14f48fdad875c925
Syed Aman <sna274@nyu.edu>
2016-11-30 21:07:33 -0500

    define login page

---
+++ login.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>PHP Login Page</title>
+    </head>
+    <body>
+        <h2>Login Page</h2>
+        <a href="index.php">Click here to go back<br><br>
+        <form>
+            Enter Username: <input type="text" name="username" required="required"> <br>
+            Enter Password: <input type="password" name="password" required="required"> <br>
+        </form>
+    </body>
+</html>
