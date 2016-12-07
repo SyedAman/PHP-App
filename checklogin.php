6b1406ee4af2578f0ac30843163b7ddcd474a2c7
Syed Aman <sna274@nyu.edu>
2016-12-07 09:31:44 -0500

    output error and exit upon server connection error

--- checklogin.php
+++ checklogin.php
@@ -7,6 +7,14 @@ session_start();
 // Connect to server and first_db database
 $mysqlServer = mysqli_connect("localhost", "root", "", "first_db");
 
+// Check connection for errors
+if ($mysqlServer->connect_errorno) {
+    // output 
+    printf("Failed to connect to server: %s\n", $mysqlServer->connect_error);
+    // exit upon connection error
+    exit();
+}
+
 // POST username and password to web server
 // Escape strings to protect against SQL injections
 $username = $mysqlServer->real_escape_string($_POST['username']);
