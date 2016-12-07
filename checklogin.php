6cb991fd3681f26f07121f3125436eb7ac1d5203
Syed Aman <sna274@nyu.edu>
2016-12-07 08:50:57 -0500

    connect to server and first_db database by default

--- checklogin.php
+++ checklogin.php
@@ -3,4 +3,10 @@
 <?php
 // create session and call read session save handler to populate $_SESSION superglobal with session date 
 session_start();
+
+// Connect to server and first_db database
+$mysqlServer = mysqli_connect("localhost", "root", "", "first_db");
+
+// POST username and password to web server
+// $username = mysq
 ?>
