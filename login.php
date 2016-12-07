b497a6e3a18068cd1976ede7b3234f7c9c957201
Syed Aman <sna274@nyu.edu>
2016-12-07 08:53:25 -0500

    close link before form

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ echo 'Current PHP version: ' . phpversion();
     </head>
     <body>
         <h2>Login Page</h2>
-        <a href="index.php">Click here to go back<br><br>
+        <a href="index.php">Click here to go back</a><br><br>
         <form>
             Enter Username: <input type="text" name="username" required="required"> <br>
             Enter Password: <input type="password" name="password" required="required"> <br>
