317dbe0b06683a4847b2d4c0c60eb20221941e08
Syed Aman <sna274@nyu.edu>
2016-12-07 09:19:18 -0500

    call checklogin.php and HTTP Post request upon login

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ echo 'Current PHP version: ' . phpversion();
     <body>
         <h2>Login Page</h2>
         <a href="index.php">Click here to go back</a><br><br>
-        <form>
+        <form action="checklogin.php" method="POST">
             Enter Username: <input type="text" name="username" required="required"> <br>
             Enter Password: <input type="password" name="password" required="required"> <br>
         </form>
